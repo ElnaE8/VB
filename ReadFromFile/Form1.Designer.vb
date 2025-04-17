4898707ca97e6253f7485a8efcae5be50a9f780f
ElnaE8 <e8construct@gmail.com>
2025-04-17 10:23:39 +1000

    Add files via upload

---
+++ ReadFromFile/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnSelectFile = New System.Windows.Forms.Button()
+        Me.lvFile = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'btnSelectFile
+        '
+        Me.btnSelectFile.Location = New System.Drawing.Point(46, 29)
+        Me.btnSelectFile.Name = "btnSelectFile"
+        Me.btnSelectFile.Size = New System.Drawing.Size(196, 35)
+        Me.btnSelectFile.TabIndex = 0
+        Me.btnSelectFile.Text = "Select File"
+        Me.btnSelectFile.UseVisualStyleBackColor = True
+        '
+        'lvFile
+        '
+        Me.lvFile.HideSelection = False
+        Me.lvFile.Location = New System.Drawing.Point(45, 89)
+        Me.lvFile.Name = "lvFile"
+        Me.lvFile.Size = New System.Drawing.Size(446, 276)
+        Me.lvFile.TabIndex = 1
+        Me.lvFile.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lvFile)
+        Me.Controls.Add(Me.btnSelectFile)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnSelectFile As Windows.Forms.Button
+    Friend WithEvents lvFile As Windows.Forms.ListView
+End Class
---
+++ ReadFromFile/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Windows.Forms
+Imports System.IO
+
+
+Public Class Form1
+    Dim fileName As String
+    Private Sub btnSelectFile_Click(sender As Object, e As EventArgs) Handles btnSelectFile.Click
+
+        Dim dialog As New OpenFileDialog()
+        With lvFile
+            .View = View.Details
+            .GridLines = True
+            .Columns.Clear()
+            .Items.Clear()
+            .Columns.Insert(0, "ID")
+            .Columns.Insert(1, "Colour")
+            .Columns.Insert(2, "Date")
+
+            .Columns(0).Width = 0
+            .Columns(1).Width = 100
+            .Columns(2).Width = 250
+            .FullRowSelect = True ' this property allows selection of the row
+            .MultiSelect = False
+        End With
+
+
+        dialog.Title = "Browse Files"
+        '   dialog.Filter = "Text Files(*.*)|*.TXT"
+        If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+            'Using MyReader As New Microsoft.VisualBasic.
+            'FileIO.TextFieldParser(
+            '            "C:\TestFolder\test.txt")
+            Using MyReader As New Microsoft.VisualBasic.
+                      FileIO.TextFieldParser(dialog.FileName)
+                MyReader.TextFieldType = FileIO.FieldType.Delimited
+                MyReader.SetDelimiters(",")
+                Dim currentRow As String()
+
+                ' First row is columns
+
+                currentRow = MyReader.ReadFields()
+                Dim currentField As String
+                For Each currentField In currentRow
+                    MsgBox(currentField)
+                Next
+
+                While Not MyReader.EndOfData
+                    Try
+                        'Read Next Line into list box
+                        currentRow = MyReader.ReadFields()
+                        '  Dim MyArray() As String = Split(currentRow.ToString, ",")
+                        lvFile.Items.Add(New ListViewItem(New String() {currentRow(1), currentRow(0), currentRow(2)}))
+
+                    Catch ex As Microsoft.VisualBasic.
+                                FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message &
+                        "is not valid and will be skipped.")
+                    End Try
+                End While
+            End Using
+        End If
+
+    End Sub
+
+
+End Class
